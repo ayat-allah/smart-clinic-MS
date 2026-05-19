87d0378133f1772f31c003568346554124710768
ayat-allah <166665397+ayat-allah@users.noreply.github.com>
2026-05-20 00:57:41 +0300

    Delete smart-clinic-managment-system directory

--- smart-clinic-managment-system/smart-clinic-managment-system/scms/scms/src/main/java/com/smartclinic/scms/entity/AppointmentType.java
+++
@@ -1,6 +0,0 @@
-package com.smartclinic.scms.entity;
-
-public enum AppointmentType {
-    EXAMINATION,
-    FOLLOW_UP
-}
--- smart-clinic-managment-system/smart-clinic-managment-system/scms/scms/src/main/java/com/smartclinic/scms/repository/UserProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartclinic.scms.repository;
-
-import com.smartclinic.scms.entity.UserProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
-}
